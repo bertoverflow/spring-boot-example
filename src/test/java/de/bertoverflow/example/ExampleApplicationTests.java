7ee253c3468bcb0df80408e15fd5ca17b38cc5c4
Bertram Vogel <bertram.vogel@codecentric.de>
2021-02-12 16:32:48 +0100

    simple spring-boot web-app

---
+++ src/test/java/de/bertoverflow/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bertoverflow.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
